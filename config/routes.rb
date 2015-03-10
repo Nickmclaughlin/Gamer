f3fa4eecc00438e46bb09d60cf4b8ad67a080eca
Nick McLaughlin <mclaughlin.nick@gmail.com>
2015-03-09 23:28:10 -0400

    1) users controller created edit update

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  resources :users, only: [:show, :index, :edit, :update]
+
+  resources :blogs  do
+    resources :comments
+
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
