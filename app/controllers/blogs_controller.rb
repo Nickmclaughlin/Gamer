9645160813174252669a1bce2b1fd6679668147f
Nick McLaughlin <mclaughlin.nick@gmail.com>
2015-03-17 20:30:49 -0400

    1) Create comments controller and model 2) Add associations 3) Able to Edit Delete Blog Posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -19,6 +19,39 @@ class BlogsController < ApplicationController
     end
   end
 
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+
+    if @blog.update(blog_params)
+      flash[:success] = "You have successfully updated your blog."
+      redirect_to blog_path(@blog)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+
+    flash[:success] = "You deleted your blog!"
+
+    redirect_to user_path(@blog.user_id)
+  end
+
+  # def destroy
+  #   @review = Review.find(params[:id])
+  #   @review.destroy
+  #
+  #   flash[:success] = "You have successfully deleted your review."
+  #
+  #   redirect_to neighborhood_path(@review.neighborhood)
+  # end
+
   private
   def blog_params
     params.require(:blog).permit(:title, :body, :genre, :user_id)
