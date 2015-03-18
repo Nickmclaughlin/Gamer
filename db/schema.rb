2ee2c595a3e061ef6c4bf639aeccbf55dcd193b5
Nick McLaughlin <mclaughlin.nick@gmail.com>
2015-03-17 20:08:02 -0400

    1) Create Migration for Comments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310033310) do
+ActiveRecord::Schema.define(version: 20150318000514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20150310033310) do
     t.string   "body"
     t.integer  "user_id"
     t.string   "genre"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "user_id"
+    t.integer  "review_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
