ae081d3204736f55815ab3ec2b90deaf3bd9b9b4
Nick McLaughlin <mclaughlin.nick@gmail.com>
2015-05-20 16:27:47 -0400

    Figuring out how to favorite and unfavorite.  Logic is making my head spin should be easy but cant figure out will come back to later

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,31 @@
 class LikesController < ApplicationController
-  
+  def create
+    @like = Like.new
+    @like.user_id = current_user.id
+    @like.team_id = params[:team_id]
+    @like.favorite = "true"
+
+    if @like.save
+      redirect_to teams_path
+    else
+      render 'teams/index'
+    end
+  end
+
+  def destroy
+    def destroy
+      @Like = Like.find(params[:id])
+      @Like.destroy
+
+      flash[:success] = "You successfully removed Team from favorites"
+
+      redirect_to teams_path
+    end
+  end
+
+  private
+  def like_params
+    params.require(:like).permit(:favorite, :team_id, :user_id)
+  end
+
 end
